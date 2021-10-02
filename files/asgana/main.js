885f0c3d674adf79d3c4b0b0db1d0e999efc4c0e
Technical Spot <90209858+techimodi@users.noreply.github.com>
2021-10-02 10:18:24 +0530

    Update main.js

--- files/asgana/main.js
+++ files/asgana/main.js
@@ -1,80 +1,80 @@
-var sngs = document.getElementById(&quot;myTable&quot;).getElementsByClassName(&quot;sngdv&quot;);
-  for (var sng = 0; sng &lt; sngs.length; sng++){
-  sngs[sng].setAttribute(&quot;id&quot;, sng);
+var sngs = document.getElementById("myTable").getElementsByClassName("sngdv");
+  for (var sng = 0; sng < sngs.length; sng++){
+  sngs[sng].setAttribute("id", sng);
   }
-  document.getElementById(&quot;srch_page&quot;).innerHTML += document.getElementById(&quot;myTable&quot;).innerHTML;
+  document.getElementById("srch_page").innerHTML += document.getElementById("myTable").innerHTML;
 function srcsng(atnm){
   filter = atnm.toUpperCase();
-  td = document.getElementById(&quot;myTable&quot;).getElementsByClassName(&quot;sngdv&quot;);
-  for (i = 0; i &lt; td.length; i++) {
+  td = document.getElementById("myTable").getElementsByClassName("sngdv");
+  for (i = 0; i < td.length; i++) {
     tdv = td[i];
     if (tdv) {
       txtValue = tdv.textContent || tdv.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) &gt; -1) {
-    var nwsng = document.createElement(&quot;div&quot;);
-    nwsng.setAttribute(&quot;class&quot;, &quot;sngdv&quot;);
-    nwsng.setAttribute(&quot;data-src&quot;, tdv.getAttribute(&quot;data-src&quot;));
-    nwsng.setAttribute(&quot;id&quot;, tdv.getAttribute(&quot;id&quot;));
-    nwsng.setAttribute(&quot;onclick&quot;, &quot;plysng(this)&quot;);
-    var nwimg = document.createElement(&quot;img&quot;);
-    nwimg.setAttribute(&quot;src&quot;, tdv.getElementsByClassName(&quot;sngimg&quot;)[0].src);
-    nwimg.setAttribute(&quot;class&quot;, &quot;sngimg&quot;);
-    var nwply = document.createElement(&quot;img&quot;);
-    nwply.setAttribute(&quot;class&quot;, &quot;play_icon&quot;);
-    nwply.setAttribute(&quot;src&quot;, &quot;https://html-code-editor.w3spaces.com/play.svg&quot;);
-    var nwpnm = document.createElement(&quot;p&quot;);
-    nwpnm.setAttribute(&quot;class&quot;, &quot;plsnm&quot;);
-    nwpnm.innerText = tdv.getElementsByClassName(&quot;plsnm&quot;)[0].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    var nwsng = document.createElement("div");
+    nwsng.setAttribute("class", "sngdv");
+    nwsng.setAttribute("data-src", tdv.getAttribute("data-src"));
+    nwsng.setAttribute("id", tdv.getAttribute("id"));
+    nwsng.setAttribute("onclick", "plysng(this)");
+    var nwimg = document.createElement("img");
+    nwimg.setAttribute("src", tdv.getElementsByClassName("sngimg")[0].src);
+    nwimg.setAttribute("class", "sngimg");
+    var nwply = document.createElement("img");
+    nwply.setAttribute("class", "play_icon");
+    nwply.setAttribute("src", "https://html-code-editor.w3spaces.com/play.svg");
+    var nwpnm = document.createElement("p");
+    nwpnm.setAttribute("class", "plsnm");
+    nwpnm.innerText = tdv.getElementsByClassName("plsnm")[0].innerText;
     nwsng.appendChild(nwimg);
     nwsng.appendChild(nwply);
     nwsng.appendChild(nwpnm);
-    document.getElementById(&quot;orcnt&quot;).appendChild(nwsng);
-    document.getElementById(&quot;artist_page&quot;).getElementsByClassName(&quot;nosngs&quot;)[0].innerHTML = document.getElementById(&quot;artist_page&quot;).getElementsByClassName(&quot;nosngs&quot;)[0].innerHTML*1+1;
+    document.getElementById("orcnt").appendChild(nwsng);
+    document.getElementById("artist_page").getElementsByClassName("nosngs")[0].innerHTML = document.getElementById("artist_page").getElementsByClassName("nosngs")[0].innerHTML*1+1;
       } else {}
     }
   }
 }
 function art(artist){
-    var artist_page = document.getElementById(&quot;artist_page&quot;);
-    artist_page.getElementsByClassName(&quot;aimg&quot;)[0].src = artist.getElementsByClassName(&quot;artimg&quot;)[0].src;
-    artist_page.getElementsByClassName(&quot;anm&quot;)[0].innerText = artist.getElementsByClassName(&quot;artnm&quot;)[0].innerText;
-    srcsng(artist.getElementsByClassName(&quot;artnm&quot;)[0].innerText);
-    document.getElementById(&quot;glow&quot;).style.display = &quot;none&quot;;
-document.getElementById(&quot;srch_page&quot;).style.display = &quot;none&quot;;
-    document.getElementById(&quot;artist_page&quot;).getElementsByClassName(&quot;nosngs&quot;)[0].innerHTML += &quot; SONGS&quot;;
- document.getElementById(&quot;artist_page&quot;).style.display = &quot;block&quot;;
+    var artist_page = document.getElementById("artist_page");
+    artist_page.getElementsByClassName("aimg")[0].src = artist.getElementsByClassName("artimg")[0].src;
+    artist_page.getElementsByClassName("anm")[0].innerText = artist.getElementsByClassName("artnm")[0].innerText;
+    srcsng(artist.getElementsByClassName("artnm")[0].innerText);
+    document.getElementById("glow").style.display = "none";
+document.getElementById("srch_page").style.display = "none";
+    document.getElementById("artist_page").getElementsByClassName("nosngs")[0].innerHTML += " SONGS";
+ document.getElementById("artist_page").style.display = "block";
 }
 function dsart(){
-    document.getElementById(&quot;artist_page&quot;).style.display = &quot;none&quot;;
+    document.getElementById("artist_page").style.display = "none";
     
- document.getElementById(&quot;glow&quot;).style.display = &quot;block&quot;;
-    document.getElementById(&quot;orcnt&quot;).innerHTML = &quot;&quot;;
-    document.getElementById(&quot;artist_page&quot;).getElementsByClassName(&quot;nosngs&quot;)[0].innerHTML = &quot;0&quot;;
+ document.getElementById("glow").style.display = "block";
+    document.getElementById("orcnt").innerHTML = ";
+    document.getElementById("artist_page").getElementsByClassName("nosngs")[0].innerHTML = "0";
 }
 var song = new Audio;
 var isStopped = true;
 
-localStorage.setItem(&quot;cntsngid&quot;, &quot;0&quot;);
-song.src = document.getElementById(&quot;0&quot;).getAttribute(&quot;data-src&quot;);
+localStorage.setItem("cntsngid", "0");
+song.src = document.getElementById("0").getAttribute("data-src");
 
 function hld(){
-    document.getElementById(&quot;load&quot;).style.display = &quot;none&quot;;
+    document.getElementById("load").style.display = "none";
 }
 
-song.addEventListener(&#39;loadstart&#39;, function(){
-    document.getElementById(&quot;load&quot;).style.display = &quot;block&quot;;
+song.addEventListener('loadstart', function(){
+    document.getElementById("load").style.display = "block";
 });
 function chkpp(){
   if(song.paused){
-  document.getElementById(&quot;pply&quot;).style.display = &quot;inline-block&quot;;
-  document.getElementById(&quot;ppse&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;ply&quot;).style.display = &quot;inline-block&quot;;
-  document.getElementById(&quot;pse&quot;).style.display = &quot;none&quot;;
+  document.getElementById("pply").style.display = "inline-block";
+  document.getElementById("ppse").style.display = "none";
+  document.getElementById("ply").style.display = "inline-block";
+  document.getElementById("pse").style.display = "none";
   } else{
-  document.getElementById(&quot;pply&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;ppse&quot;).style.display = &quot;inline-block&quot;;
-  document.getElementById(&quot;ply&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;pse&quot;).style.display = &quot;inline-block&quot;;
+  document.getElementById("pply").style.display = "none";
+  document.getElementById("ppse").style.display = "inline-block";
+  document.getElementById("ply").style.display = "none";
+  document.getElementById("pse").style.display = "inline-block";
   }
   }
 function playpause(){
@@ -89,38 +89,38 @@ function playpause(){
 
 function plysng(plsng){
   song.pause();
-  song.src = plsng.getAttribute(&quot;data-src&quot;);
+  song.src = plsng.getAttribute("data-src");
   song.play();
   ply();
-  songName = document.getElementById(&quot;songName&quot;);
-  songName.innerHTML = plsng.getElementsByClassName(&quot;plsnm&quot;)[0].innerText;
-var sngnum = plsng.getAttribute(&quot;id&quot;);
-localStorage.setItem(&quot;cntsngid&quot;, sngnum);
-    document.getElementById(&quot;btnsngnm&quot;).innerHTML = plsng.getElementsByClassName(&quot;plsnm&quot;)[0].innerText;
-	document.getElementById(&quot;btmimg&quot;).src = plsng.getElementsByClassName(&quot;sngimg&quot;)[0].src;
-	document.getElementById(&quot;dftimg&quot;).src = plsng.getElementsByClassName(&quot;sngimg&quot;)[0].src;
-	document.getElementById(&quot;btmnu&quot;).style.display = &quot;block&quot;;
+  songName = document.getElementById("songName");
+  songName.innerHTML = plsng.getElementsByClassName("plsnm")[0].innerText;
+var sngnum = plsng.getAttribute("id");
+localStorage.setItem("cntsngid", sngnum);
+    document.getElementById("btnsngnm").innerHTML = plsng.getElementsByClassName("plsnm")[0].innerText;
+	document.getElementById("btmimg").src = plsng.getElementsByClassName("sngimg")[0].src;
+	document.getElementById("dftimg").src = plsng.getElementsByClassName("sngimg")[0].src;
+	document.getElementById("btmnu").style.display = "block";
 }
 
 function skip(to) {
-	if (to == &#39;prev&#39;) {
-	  if (localStorage.getItem(&quot;cntsngid&quot;) &gt; 0){
-	  var plyprnm = localStorage.getItem(&quot;cntsngid&quot;);
+	if (to == 'prev') {
+	  if (localStorage.getItem("cntsngid") > 0){
+	  var plyprnm = localStorage.getItem("cntsngid");
 	  var reqbtnid = (plyprnm*1 - 1*1);
 	  document.getElementById(reqbtnid).click();
 	  song.play();
 	  } else{
-	      alert(&quot;This is the first music.\nClick on the next button to play next.&quot;);
+	      alert("This is the first music.\nClick on the next button to play next.");
 	  }
 	}
-	else if (to == &#39;next&#39;) {
-		if (localStorage.getItem(&quot;cntsngid&quot;) &gt; -1){
-		var plyprnm = localStorage.getItem(&quot;cntsngid&quot;);
+	else if (to == 'next') {
+		if (localStorage.getItem("cntsngid") > -1){
+		var plyprnm = localStorage.getItem("cntsngid");
 		var reqbtnid = (plyprnm*1 + 1*1);
 		document.getElementById(reqbtnid).click();
 		song.play();
 		} else{
-		document.getElementById(&quot;0&quot;).click();
+		document.getElementById("0").click();
 		}
 	}
 }
@@ -128,19 +128,19 @@ function skip(to) {
 function stop() {
 	song.pause();
 	song.currentTime = 0;
-	document.getElementById(&quot;seek&quot;).value = 0;
+	document.getElementById("seek").value = 0;
 	isStopped = true;
 }
 
 function tmvl(){
     var cutime = parseInt(song.currentTime,10);
-	var ltstm = Math.floor(song.duration/60)+&#39;:&#39;+Math.floor(song.duration)%60;
-	document.getElementById(&#39;ltstm&#39;).innerHTML = &#39;  &#39;+ltstm;
-	if (cutime &gt; 59){
-	var ftstm = Math.floor(cutime/60)+&#39;:&#39;+Math.floor(cutime)%60;
-	document.getElementById(&#39;ftstm&#39;).innerHTML = ftstm;
+	var ltstm = Math.floor(song.duration/60)+':'+Math.floor(song.duration)%60;
+	document.getElementById('ltstm').innerHTML = '  '+ltstm;
+	if (cutime > 59){
+	var ftstm = Math.floor(cutime/60)+':'+Math.floor(cutime)%60;
+	document.getElementById('ftstm').innerHTML = ftstm;
 	} else{
-	document.getElementById(&#39;ftstm&#39;).innerHTML = &#39;0:&#39;+cutime+&#39;  &#39;;
+	document.getElementById('ftstm').innerHTML = '0:'+cutime+'  ';
 	}
 }
 
@@ -150,104 +150,104 @@ function setPos(pos) {
 
 function main(){
   chkpp();
-  document.getElementById(&quot;player&quot;).classList.add(&quot;active&quot;);
-  document.getElementById(&quot;back&quot;).style.display = &quot;block&quot;;
-  document.getElementById(&quot;body&quot;).style.overflow = &quot;hidden&quot;;
+  document.getElementById("player").classList.add("active");
+  document.getElementById("back").style.display = "block";
+  document.getElementById("body").style.overflow = "hidden";
 }
 function del(){
   chkpp();
-  document.getElementById(&quot;player&quot;).classList.remove(&quot;active&quot;);
-  document.getElementById(&quot;back&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;body&quot;).style.overflow = &quot;scroll&quot;;
+  document.getElementById("player").classList.remove("active");
+  document.getElementById("back").style.display = "none";
+  document.getElementById("body").style.overflow = "scroll";
 }
 function myFunction() {
   var input, myTable, filter, td, arts, i, a, txtValue, artVl;
-  input = document.getElementById(&quot;myInput&quot;);
-  myTable = document.getElementById(&quot;srch_page&quot;);
+  input = document.getElementById("myInput");
+  myTable = document.getElementById("srch_page");
   filter = input.value.toUpperCase();
-  arts = myTable.getElementsByClassName(&quot;artdv&quot;);
-  td = myTable.getElementsByClassName(&quot;sngdv&quot;);
+  arts = myTable.getElementsByClassName("artdv");
+  td = myTable.getElementsByClassName("sngdv");
   if (filter != null){
-  for (i = 0; i &lt; td.length; i++) {
+  for (i = 0; i < td.length; i++) {
     tdv = td[i];
     if (tdv) {
       txtValue = tdv.textContent || tdv.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) &gt; -1) {
-        tdv.style.display = &quot;inline-block&quot;;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tdv.style.display = "inline-block";
       } else {
-        tdv.style.display = &quot;none&quot;;
+        tdv.style.display = "none";
       }
     }
   }
-  for (a = 0; a &lt; arts.length; a++){
+  for (a = 0; a < arts.length; a++){
     art = arts[a];
     if (art) {
-      artVl = art.getElementsByClassName(&quot;artnm&quot;)[0].innerText;
-      if (artVl.toUpperCase().indexOf(filter) &gt; -1) {
-        art.style.display = &quot;inline-block&quot;;
+      artVl = art.getElementsByClassName("artnm")[0].innerText;
+      if (artVl.toUpperCase().indexOf(filter) > -1) {
+        art.style.display = "inline-block";
       } else {
-        art.style.display = &quot;none&quot;;
+        art.style.display = "none";
       }
     }
   }
   }
 }
 
-song.addEventListener(&#39;error&#39;, function(){
+song.addEventListener('error', function(){
 	stop();
-	document.getElementById(&quot;songName&quot;).innerHTML = &quot;Error Loading Audio!&quot;;
-  document.getElementById(&quot;btnsngnm&quot;).innerHTML = &quot;Error Loading Audio!&quot;;
+	document.getElementById("songName").innerHTML = "Error Loading Audio!";
+  document.getElementById("btnsngnm").innerHTML = "Error Loading Audio!";
 });
 
-song.addEventListener(&#39;timeupdate&#39;, function() {
+song.addEventListener('timeupdate', function() {
 	curtime = parseInt(song.currentTime,10);
-	document.getElementById(&#39;seek&#39;).max = song.duration;
-	document.getElementById(&#39;seek&#39;).value = curtime;
-    document.getElementById(&quot;load&quot;).style.display = &quot;none&quot;;
+	document.getElementById('seek').max = song.duration;
+	document.getElementById('seek').value = curtime;
+    document.getElementById("load").style.display = "none";
 	tmvl();
 });
 
-song.addEventListener(&quot;ended&quot;, function() {
-	var plyprnm = localStorage.getItem(&quot;cntsngid&quot;);
+song.addEventListener("ended", function() {
+	var plyprnm = localStorage.getItem("cntsngid");
 	var reqbtnid = (plyprnm*1 + 1*1);
 	song.currentTime = 0;
-	document.getElementById(&#39;seek&#39;).value = 0;
+	document.getElementById('seek').value = 0;
 	document.getElementById(reqbtnid).click();
 	song.play();
 });
 function ply(){
   chkpp();
   song.play();
-  document.getElementById(&quot;pply&quot;).style.display = &quot;none&quot;;
+  document.getElementById("pply").style.display = "none";
 
-  document.getElementById(&quot;ppse&quot;).style.display = &quot;inline-block&quot;;
-  document.getElementById(&quot;ply&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;pse&quot;).style.display = &quot;inline-block&quot;;
+  document.getElementById("ppse").style.display = "inline-block";
+  document.getElementById("ply").style.display = "none";
+  document.getElementById("pse").style.display = "inline-block";
   }
 function pse(){
   chkpp();
   song.pause();
-  document.getElementById(&quot;pply&quot;).style.display = &quot;inline-block&quot;;
+  document.getElementById("pply").style.display = "inline-block";
 
-  document.getElementById(&quot;ppse&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;pse&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;ply&quot;).style.display = &quot;inline-block&quot;;
+  document.getElementById("ppse").style.display = "none";
+  document.getElementById("pse").style.display = "none";
+  document.getElementById("ply").style.display = "inline-block";
   }
 function menu(){
-  document.getElementById(&quot;menu&quot;).classList.add(&quot;active&quot;);
-  document.getElementById(&quot;menubk&quot;).style.display = &quot;block&quot;;
+  document.getElementById("menu").classList.add("active");
+  document.getElementById("menubk").style.display = "block";
   }
   function ofmenu(){
-  document.getElementById(&quot;menu&quot;).classList.remove(&quot;active&quot;);
-  document.getElementById(&quot;menubk&quot;).style.display = &quot;none&quot;;
+  document.getElementById("menu").classList.remove("active");
+  document.getElementById("menubk").style.display = "none";
   }
 function srch(){
-  document.getElementById(&quot;glow&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;srch_page&quot;).style.display = &quot;block&quot;;
+  document.getElementById("glow").style.display = "none";
+  document.getElementById("srch_page").style.display = "block";
   
-document.getElementById(&quot;myInput&quot;).focus();
+document.getElementById("myInput").focus();
   }
 function ofsrch(){
-  document.getElementById(&quot;srch_page&quot;).style.display = &quot;none&quot;;
-  document.getElementById(&quot;glow&quot;).style.display = &quot;block&quot;;
+  document.getElementById("srch_page").style.display = "none";
+  document.getElementById("glow").style.display = "block";
   }
