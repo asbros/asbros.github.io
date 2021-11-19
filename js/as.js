2cbfa79914bc1ed43f6dbdde03d0fb8853299a96
Technical Spot <90209858+techimodi@users.noreply.github.com>
2021-11-19 13:13:40 +0530

    Update as.js

--- js/as.js
+++ js/as.js
@@ -190,6 +190,6 @@ const as = {}
  as.init = function(){
   as.container();
   as.button();
-  javascript:(function () { var script = document.createElement('script'); script.src="https://unpkg.com/draggabilly@2/dist/draggabilly.pkgd.js"; document.body.appendChild(script); script.onload = function () {let draggie = new Draggabilly('#asbtn', {containment: true});} })();
+  javascript:(function () { var script = document.createElement('script'); script.src="https://unpkg.com/draggabilly@2/dist/draggabilly.pkgd.js"; document.body.appendChild(script); script.onload = function () {let draggie = new Draggabilly('#asbtn', {containment: !0});} })();
  }
  as.init();
