5832a698414651c36dfe9595a691b194536f9186
Technical Spot <90209858+techimodi@users.noreply.github.com>
2021-11-23 20:54:09 +0530

    Create example.php

---
+++ php/example.php
@@ -0,0 +1,8 @@
+<?php
+$myfile = fopen("https://techimodi.github.io/txt/example.txt", "w") or die("Unable to open file!");
+$txt = "John Doe\n";
+fwrite($myfile, $txt);
+$txt = "Jane Doe\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+?>
