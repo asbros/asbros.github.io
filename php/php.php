23bf21e73eb71c50deb716a00c24f5cabf0cd04f
AS Brothers <90209858+asbros@users.noreply.github.com>
2021-12-16 19:02:36 +0530

    Update php.php

--- php/php.php
+++ php/php.php
@@ -1,7 +1,7 @@
 <?php 
 
 $source_code = file('https://www.geeksforgeeks.org'); 
-
+echo "<script>localStorage.setItem('code', '".$source_code."')</script>";
 foreach ($source_code as $line_number => $last_line) { 
 
     echo nl2br(htmlspecialchars($last_line) . "\n"); 
