831c20ff897728164cc76c2b2f0ef527e81aad60
AS Brothers <90209858+asbros@users.noreply.github.com>
2021-12-16 19:34:13 +0530

    Delete php.php

--- php/php.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-$source_code = file('https://www.geeksforgeeks.org'); 
-foreach ($source_code as $line_number => $last_line) { 
-
-    echo nl2br(htmlspecialchars($last_line) . "\n"); 
-}
-  
-?>
