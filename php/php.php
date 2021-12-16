49180f48f3fa66cf0254b590705b40b6439b3e6d
AS Brothers <90209858+asbros@users.noreply.github.com>
2021-12-16 16:52:11 +0530

    Create php.php

---
+++ php/php.php
@@ -0,0 +1,10 @@
+<?php 
+
+$source_code = file('https://www.geeksforgeeks.org'); 
+
+foreach ($source_code as $line_number => $last_line) { 
+
+    echo nl2br(htmlspecialchars($last_line) . "\n"); 
+} 
+  
+?>
