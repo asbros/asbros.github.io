b5fc00d7950e4c086d2234dfd5aecd1fecc19365
AS Brothers <90209858+asbros@users.noreply.github.com>
2021-12-31 15:10:48 +0530

    Delete as-voice-commands.js

--- js/as-voice-commands.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement("script");
-let as_commands;
-script.setAttribute("src", "https://asbros.github.io/js/as-voice-commands-annyang.js");
-document.body.appendChild(script);
-script.onload = function(){as_commands = annyang;};
