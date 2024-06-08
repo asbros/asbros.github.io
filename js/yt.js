c832e643245d25cbeb993189901119770e436ce5
AS BROTHERS <90209858+asbros@users.noreply.github.com>
2024-06-08 20:17:14 +0530

    Update yt.js

--- js/yt.js
+++ js/yt.js
@@ -3,7 +3,7 @@ import { VidstackPlayer, VidstackPlayerLayout } from 'https://cdn.vidstack.io/pl
   const player = await VidstackPlayer.create({
     target: '#v_video',
     title: document.title,
-    src: src,
+    src: sessionStorage.getItem("src"),
     poster: 'https://files.vidstack.io/sprite-fight/poster.webp',
     layout: new VidstackPlayerLayout({
       thumbnails: ''
